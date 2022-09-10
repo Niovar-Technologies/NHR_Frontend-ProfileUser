5a7378bb2ac2932bd8ea5c987193a62d4e2c66bf
unknown <yaosoft@hotmail.com>
2022-09-10 02:03:35 +0100

    commit

--- src/Components/ProfileUser.js
+++ src/Components/ProfileUser.js
@@ -176,28 +176,28 @@ const ProfileUser = () => {
                             <form>
                               <div className="form-group">
                                 <label for="inputName">Name</label>
-                                <input type="text" className="form-control" id="inputName" placeholder="Name">
+                                <input type="text" className="form-control" id="inputName" placeholder="Name" />
                               </div>
                                 <div className="form-group">
                                 <label for="inputLastName">Last Name</label>
-                                <input type="text" className="form-control" id="inputLastName" placeholder="Last Name">
+                                <input type="text" className="form-control" id="inputLastName" placeholder="Last Name" />
                               </div>
                               <div className="form-group">
                                 <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email">
+                                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
                               </div>
                               <div className="form-group">
                                 <label for="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password">
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                               </div>
                               <div className="form-group">
                                 <label for="exampleInputFile">File input</label>
-                                <input type="file" id="exampleInputFile">
+                                <input type="file" id="exampleInputFile" />
                                 <p className="help-block">Example block-level help text here.</p>
                               </div>
                               <div className="checkbox">
                                 <label>
-                                  <input type="checkbox"> Check me out
+                                  <input type="checkbox" /> Check me out
                                 </label>
                               </div>
                               <button type="submit" className="btn btn-default">Submit</button>
