7826574ca4928da126f1537a096855ddb1f0f03e
unknown <yaosoft@hotmail.com>
2022-09-11 19:26:46 +0100

    test

--- src/Components/ProfileUser.js
+++ src/Components/ProfileUser.js
@@ -237,7 +237,7 @@ const ProfileUser = () => {
                         
                             <div className="col-md-6">
                                 <label className="small mb-1" for="dateEmbauche">Date d'embauche</label>
-                                <DatePicker className="form-control" id="dateEmbauche" selected={startDateEmbauche} onChange={(date) => setStartDateEmbauche(date)} />
+                                <DatePicker  locale="fr" className="form-control" id="dateEmbauche" selected={startDateEmbauche} onChange={(date) => setStartDateEmbauche(date)} />
                             </div>
                            
                             <div className="col-md-6">
