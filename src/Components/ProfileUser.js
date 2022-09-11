762a4a2f2852fed88b479ef9bb2de2d2d9c244de
unknown <yaosoft@hotmail.com>
2022-09-11 01:01:09 +0100

    commit

--- src/Components/ProfileUser.js
+++ src/Components/ProfileUser.js
@@ -126,55 +126,83 @@ const history = useHistory();
                     <form>
                         
                         <div className="mb-3">
-                            <label className="small mb-1" for="inputUsername">Username (how your name will appear to other users on the site)</label>
-                            <input className="form-control" id="inputUsername" type="text" placeholder="Enter your username" value="username" />
+                            <label className="small mb-1" for="inputUsername">Nom complet</label>
+                            <input className="form-control" id="inputUsername" type="text" placeholder="Votre nom complet" value="username" />
                         </div>
                         
                         <div className="row gx-3 mb-3">
                             
                             <div className="col-md-6">
-                                <label className="small mb-1" for="inputFirstName">First name</label>
-                                <input className="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie" />
+                                <label className="small mb-1" for="inputFirstName">Email</label>
+                                <input className="form-control" id="inputFirstName" type="text" placeholder="Votre adresse email" value="Email" />
                             </div>
                            
                             <div className="col-md-6">
-                                <label className="small mb-1" for="inputLastName">Last name</label>
-                                <input className="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna" />
+                                <label className="small mb-1" for="inputLastName">Téléphone</label>
+                                <input className="form-control" id="inputLastName" type="text" placeholder="Votre Téléphone" value="Téléphone" />
                             </div>
                         </div>
                        
                         <div className="row gx-3 mb-3">
                            
                             <div className="col-md-6">
-                                <label className="small mb-1" for="inputOrgName">Organization name</label>
-                                <input className="form-control" id="inputOrgName" type="text" placeholder="Enter your organization name" value="Start Bootstrap" />
+                                <label className="small mb-1" for="inputOrgName">Téléphone domicile</label>
+                                <input className="form-control" id="inputOrgName" type="text" placeholder="Téléphone de votre domicile" value="Start Bootstrap" />
                             </div>
                            
                             <div className="col-md-6">
                                 <label className="small mb-1" for="inputLocation">Location</label>
-                                <input className="form-control" id="inputLocation" type="text" placeholder="Enter your location" value="San Francisco, CA" />
+                                <input className="form-control" id="inputLocation" type="text" placeholder="Département" value="Département" />
                             </div>
                         </div>
                       
                         <div className="mb-3">
-                            <label className="small mb-1" for="inputEmailAddress">Email address</label>
-                            <input className="form-control" id="inputEmailAddress" type="email" placeholder="Enter your email address" value="name@example.com" />
+                            <label className="small mb-1" for="inputEmailAddress">Poste</label>
+                            <input className="form-control" id="inputEmailAddress" type="email" placeholder="Poste" value="Poste" />
                         </div>
                         
                         <div className="row gx-3 mb-3">
                         
                             <div className="col-md-6">
-                                <label className="small mb-1" for="inputPhone">Phone number</label>
-                                <input className="form-control" id="inputPhone" type="tel" placeholder="Enter your phone number" value="555-123-4567" />
+                                <label className="small mb-1" for="inputPhone">Type de salaire</label>
+                                <input className="form-control" id="inputPhone" type="tel" placeholder="Type de salaire" value="0" />
                             </div>
                            
                             <div className="col-md-6">
-                                <label className="small mb-1" for="inputBirthday">Birthday</label>
-                                <input className="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Enter your birthday" value="06/10/1988" />
+                                <label className="small mb-1" for="inputBirthday">Salaire</label>
+                                <input className="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Salaire" value="06/10/1988" />
                             </div>
                         </div>
-                      
-                        <button className="btn btn-primary" type="button">Save changes</button>
+						<div className="row gx-3 mb-3">
+                        
+                            <div className="col-md-6">
+                                <label className="small mb-1" for="inputPhone">Date d'embauche</label>
+                                <input className="form-control" id="inputPhone" type="tel" placeholder="Type de salaire" value="0" />
+                            </div>
+                           
+                            <div className="col-md-6">
+                                <label className="small mb-1" for="inputBirthday">Date de départ</label>
+                                <input className="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Salaire" value="06/10/1988" />
+                            </div>
+                        </div>
+						<div className="row gx-3 mb-3">
+                        
+                            <div className="col-md-6">
+                                <label className="small mb-1" for="inputPhone">Date d'embauche</label>
+                                <input className="form-control" id="inputPhone" type="tel" placeholder="Type de salaire" value="0" />
+                            </div>
+                           
+                            <div className="col-md-6">
+                                <label className="small mb-1" for="inputBirthday">Date de départ</label>
+                                <input className="form-control" id="inputBirthday" type="text" name="birthday" placeholder="Salaire" value="06/10/1988" />
+                            </div>
+                        </div>
+						<div className="mb-3">
+                            <label className="small mb-1" for="inputEmailAddress">Numéro d'employé</label>
+                            <input className="form-control" id="inputEmailAddress" type="email" placeholder="Poste" value="Poste" />
+                        </div>
+					  
+                        <button className="btn btn-primary" type="button">Enregistrer</button>
                     </form>
                 </div>
             </div>
