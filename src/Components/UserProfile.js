b996a14c40b1094338f8513272fcea355291536d
unknown <yaosoft@hotmail.com>
2022-09-26 09:05:08 +0100

    commit

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -484,27 +484,27 @@ console.log('save user profile');
 			validation = validationPass; 
 		
 		// sexeId
-		if( !sexeId )
+		if( sexeId == "" )
 			validation = "Homme ou femme?"; 
 		
 		// departementId
-		if( !departementId )
+		if( departementId == "" )
 			validation = "Choisissez un Département de travail?"; 
 		
 		// posteId
-		if( !posteId )
+		if( posteId == "" )
 			validation = "Choisissez un Poste de travail"; 
 		
 		// paysId
-		if( !paysId )
+		if( paysId == "" )
 			validation = "Choisissez un Pays"; 
 		
 		// provinceId
-		if( !provinceId )
+		if( provinceId == "" )
 			validation = "Choisissez une Province";
 		
 		// villeId
-		if( !villeId )
+		if( villeId == "" )
 			validation = "Choisissez une Ville";
 		
 		return validation;
